82dd0890e2c12e43e0ece0506b053978367f966a
mm1746t <vrodldh46ttt@gmail.com>
2018-07-10 10:09:43 +0900

    センサの関数

--- Src/driver.c
+++ Src/driver.c
@@ -12,4 +12,16 @@ motorParam_t motorParamList[MOTOR_NUM] = {
   },
 };
 
-
+int readSensor(sensorKind_t kind)
+{
+  
+  if(kind == LEFT){
+    if(ad_read(1) < 128) return 1;
+    else return 0;
+  }
+  if(kind == RIGHT){
+    if(ad_read(2) < 128) return 1;
+    else return 0;
+  }
+  
+}
