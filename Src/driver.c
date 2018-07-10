68730b42d398602db7f6c60a4898095f3761bd93
j15408 <j15408@fukuoka.j.kisarazu.ac.jp>
2018-07-10 10:02:56 +0900

    モ－タ制御を実装

--- Src/driver.c
+++ Src/driver.c
@@ -1,5 +1,6 @@
 /*driver.c*/
-
+#include"../Inc/driver.h"
+void driveMotor(driveMode_t, int);
 /*各モータのパラメータを初期化*/
 motorParam_t motorParamList[MOTOR_NUM] = {
   {
@@ -12,4 +13,44 @@ motorParam_t motorParamList[MOTOR_NUM] = {
   },
 };
 
+void driveMotor(driveMode_t motor, int move){
+  swich(motor){
+    case STOP:
+    if(move == MOTOR_L){
+      PB0 = 0;
+      PB1 = 0;
+    }else if(move == MOTOR_R){
+      PB2 = 0
+      PB3 = 0
+    }
+    break;
+    case FORWARD:
+    if(move == MOTOR_L){
+      PB0 = 1;
+      PB1 = 0;
+    }else if(move == MOTOR_R){
+      PB2 = 1;
+      PB3 = 0;
+    }
+    break;
+    case BAKCWARD:
+    if(move == MOTOR_L){
+      PB0 = 0;
+      PB1 = 1;
+    }else if(move == MOTOR_R){
+      PB2 = 0;
+      PB3 = 1;
+    }
+    break;
+    case BRAKE:
+    if(move == MOTOR_L){
+      PB0 = 1;
+      PB1 = 1;
+    }else if(move == MOTOR_R){
+      PB2 = 1;
+      PB3 = 1;
+    }
+    break;
+  }
 
+}
