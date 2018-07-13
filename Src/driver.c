fafd4ba50dad9435fefdfab774d6bdfdb416f1d2
j15426 <j15426@fukuoka.j.kisarazu.ac.jp>
2018-07-13 10:18:10 +0900

    モータ制御関数の変数を修正

--- Src/driver.c
+++ Src/driver.c
@@ -1,6 +1,8 @@
 /*driver.c*/
+#include "h8-3069-iodef.h"
 
 #include "driver.h"
+#include "ad_read.h"
 
 /*各モータのパラメータを初期化*/
 motorParam_t motorParamList[MOTOR_NUM] = {
@@ -16,7 +18,7 @@ motorParam_t motorParamList[MOTOR_NUM] = {
 
 int readSensor(sensorKind_t kind)
 {
-  
+
   if(kind == LEFT){
     if(ad_read(1) < 128) return 1;
     else return 0;
@@ -30,41 +32,37 @@ int readSensor(sensorKind_t kind)
 }
 
 void driveMotor(driveMode_t motor, int move){
-  swich(motor){
+  switch(motor){
     case STOP:
     if(move == MOTOR_L){
-      PB0 = 0;
-      PB1 = 0;
+      PBDR &= 0xfc;
     }else if(move == MOTOR_R){
-      PB2 = 0
-      PB3 = 0
+      PBDR &= 0xf3;
+    }
+    break;
+    case BACKWARD:
+    if(move == MOTOR_L){
+      PBDR &= 0xfd;
+      PBDR |= 0x01;
+    }else if(move == MOTOR_R){
+      PBDR &= 0xf7;
+      PBDR |= 0x04;
     }
     break;
     case FORWARD:
     if(move == MOTOR_L){
-      PB0 = 1;
-      PB1 = 0;
+      PBDR &= 0xfe;
+      PBDR |= 0x02;
     }else if(move == MOTOR_R){
-      PB2 = 1;
-      PB3 = 0;
-    }
-    break;
-    case BAKCWARD:
-    if(move == MOTOR_L){
-      PB0 = 0;
-      PB1 = 1;
-    }else if(move == MOTOR_R){
-      PB2 = 0;
-      PB3 = 1;
+      PBDR &= 0xfb;
+      PBDR |= 0x08;
     }
     break;
     case BRAKE:
     if(move == MOTOR_L){
-      PB0 = 1;
-      PB1 = 1;
+      PBDR |= 0x03;
     }else if(move == MOTOR_R){
-      PB2 = 1;
-      PB3 = 1;
+      PBDR |= 0x0c;
     }
     break;
   }
