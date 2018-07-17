67f253ad7f7a80981133a3edf8312a972f500833
chuyii <contact@chuyii.net>
2018-07-17 10:07:59 +0900

    コントロールの修正

--- Src/control.c
+++ Src/control.c
@@ -4,7 +4,7 @@
 
 void controlMotor(void){
 
-  if(readSensor(LEFT)){
+  if(readSensor(RIGHT)){
     motorParamList[MOTOR_L].duty = 60;
     motorParamList[MOTOR_L].mode = FORWARD;
   }else{
@@ -12,7 +12,7 @@ void controlMotor(void){
     motorParamList[MOTOR_L].mode = STOP;
   }
 
-  if(readSensor(RIGHT)){
+  if(readSensor(LEFT)){
     motorParamList[MOTOR_R].duty = 60;
     motorParamList[MOTOR_R].mode = FORWARD;
   }else{
